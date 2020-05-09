bd493597aac750763f380f99dedfd2d6996ebb73
Ted Neben <tedneben@gmail.com>
2020-05-09 00:15:38 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0 
   
   while count < array.length do
-    array.length[count] == value_to_find
+    puts array[count] == value_to_find
     count += 1 
   end
 end
